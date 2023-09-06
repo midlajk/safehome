4854587a60dd6e94bf5b2e6515a9c93ef4ea8356
midlaj <midlajk18@gmail.com>
2023-09-06 19:33:29 +0530

    data ytable error check v7

--- middleware/isadminapi.js
+++ middleware/isadminapi.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   console.log(req.session.isadminlogged)
     if (!req.session.isadminlogged) {
+      console.log('herer')
         return res.status(500).json({error: 'Server login error.'});
     }
     else{

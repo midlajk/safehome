b4dc1178f0160ffe6cea0cefd16479a7b274f85e
midlaj <midlajk18@gmail.com>
2023-10-02 22:02:35 +0530

    local test ok v3

--- app.js
+++ app.js
@@ -45,12 +45,12 @@ app.use(
 // view engine setup
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-const corsOptions ={
-  origin:'https://www.safehomes.ae', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//   origin:'https://www.safehomes.ae', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

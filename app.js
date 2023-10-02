8a57632980e72903930b750d6e984f245901fb7f
midlaj <midlajk18@gmail.com>
2023-10-02 22:57:54 +0530

    bosy-barser

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ app.set('views', 'views');
 
 
 app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());

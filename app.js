53f61487e7b54b9e1ac835be0fca6fccda13c7c3
midlaj <midlajk18@gmail.com>
2023-10-02 22:10:18 +0530

    local test ok v4

--- app.js
+++ app.js
@@ -45,12 +45,14 @@ app.use(
 // view engine setup
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-// const corsOptions ={
-//   origin:'https://www.safehomes.ae', 
-//   credentials:true,            //access-control-allow-credentials:true
-//   optionSuccessStatus:200
-// }
-app.use(cors());
+const corsOptions = {
+  origin: 'https://www.safehomes.ae', 
+  credentials: true, // Use camel-cased 'credentials'
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -77,6 +79,10 @@ app.use('/', frontendapi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://safehomes.ae'); // Replace with your frontend's origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   next(createError(404));
 });
 

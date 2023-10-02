701e980af421d53e5715bc44e7672860931b9763
midlaj <midlajk18@gmail.com>
2023-10-02 14:02:55 +0530

    version 5

--- model/db.js
+++ model/db.js
@@ -8,8 +8,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const password = 'dev$7e3';
 const encodedPassword = encodeURIComponent(password);
 // const url = `mongodb://safehomes%40gmail.com:safehomes123%23@127.0.0.1:27017/safehome`;
-const url = `mongodb://127.0.0.1:27017/safehome`;
-
+const url = `mongodb://safehomes%40gmail.com:safehomes123%23@127.0.0.1:27017/safehome`;
 // Connect to the bulbous database
 mongoose.connect(url, { serverApi: ServerApiVersion.v1 });
 //Connect methode of mongoose
